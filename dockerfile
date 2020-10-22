f051d8d02ed69319d4ffcd583496b821f7c6f8a7
RaquelMnascimento <raquel.mnascimento@outlook.com>
2020-10-22 19:56:08 -0300

    Ac4

--- dockerfile
+++ dockerfile
@@ -1,7 +1,6 @@
  From python:3.6.1-alpine
  RUN pip install flask
- COPY app.py /app.py
- CMD ["python", "app.py"]
-
+ COPY primos.py /primos.py
+ CMD ["python", "primos.py"}
 
 
--- primos.py
+++ primos.py
@@ -2,35 +2,31 @@ import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route('/')
-def nao_entre_em_panico():
+def sequencia_fibonacci():
+   proximo = 1
+   anterior = 0
+   limite = 100
+   found = 0
+   resposta = "0,"
+   while (found < limite):
+       found=found+1 
+       if (found):
+           tmp = proximo
+           proximo = proximo + anterior
+           anterior = tmp
+           resposta+= str(proximo) + ","
+           if (found % 5 == 0):
+               resposta = resposta + "<br>" 
 
-    maximo = 100
+   return resposta
 
-        
-    a = 1
-    cont = 1
-    valor = 3
-        
-    primos = "2,"
-    
-    
-    while cont < maximo:
-        primo = 1
-        for p in range (2, valor):
-            if valor % p == 0:
-                primo = 0
-                break 
-        if(primo):
-            primos = primos + str(valor) + ","
-            cont += 1
-            if(cont % 10 == 0):
-                primos = primos + "<br>"
-        valor+=1
-    return primos
-
-if __name__ == "__main__":
+if _name_ == "_main_":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)  
+    app.run(host='0.0.0.0', port=port)
+
+
+
+
