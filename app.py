39456d0c6413a46a58baf8c56cb25788cb1c05f0
RaquelMnascimento <raquel.mnascimento@outlook.com>
2020-10-27 08:19:21 -0300

    test

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def sequencia_fibonacci():
@@ -23,6 +23,6 @@ def sequencia_fibonacci():
 
    return resposta
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
--- dockerfile
+++ dockerfile
@@ -1,6 +1,6 @@
- From python:3.6.1-alpine
+ FROM python:3.6.1-alpine
  RUN pip install flask
- COPY primos.py /primos.py
- CMD ["python", "primos.py"}
+ COPY app.py /app.py
+ CMD ["python", "app.py"}
 
 
